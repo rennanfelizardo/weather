5d99b487330b5829ece16866788d043b53eb2ace
Rennan Felizardo <rennan.felizardo@dce.ufpb.br>
2016-10-14 14:49:30 -0300

    first commit

--- app/src/main/java/weather/rennanfelizardo/com/br/weather/models/Coord.java
+++ app/src/main/java/weather/rennanfelizardo/com/br/weather/models/Coord.java
@@ -15,38 +15,18 @@ public class Coord {
     @Expose
     private Double lat;
 
-    /**
-     *
-     * @return
-     *     The lon
-     */
     public Double getLon() {
         return lon;
     }
 
-    /**
-     *
-     * @param lon
-     *     The lon
-     */
     public void setLon(Double lon) {
         this.lon = lon;
     }
 
-    /**
-     *
-     * @return
-     *     The lat
-     */
     public Double getLat() {
         return lat;
     }
 
-    /**
-     *
-     * @param lat
-     *     The lat
-     */
     public void setLat(Double lat) {
         this.lat = lat;
     }
